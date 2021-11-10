41a29787a693984a34cc6a850cca574899b7c703
Enock Chikobvu <sechikobvu@gmail.com>
2021-11-10 11:49:12 +0200

    escape

--- script/script.js
+++ script/script.js
@@ -6,14 +6,14 @@ $(document).ready(() => {
   });
 
   $(window).on('load scroll', () => {
-     $('.fa-bars').removeClass('fa-times');
+     //$('.fa-bars').removeClass('fa-times');
      $('.navbar').removeClass('nav-toggle')
 
-     /*if($(window).scrollTop() > 30){
+     if($(window).scrollTop() > 30){
         $('.header').css({'background':'#6C5CE7','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4'});
      } else{
       $('.header').css({'background':'none','box-shadow':'none'});
-     }*/
+     }
   });
 
 
