03f9f5467a219c3f703ca11eb9fe67fca332deb3
Enock Chikobvu <sechikobvu@gmail.com>
2021-11-10 12:06:26 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
 
   $(window).on('load scroll', () => {
      $('.fa-bars').removeClass('fa-times');
-     //$('.navbar').removeClass('nav-toggle')
+     $('.navbar').removeClass('nav-toggle')
 
      if($(window).scrollTop() > 30){
         $('.header').css({'background':'#6C5CE7','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4'});
